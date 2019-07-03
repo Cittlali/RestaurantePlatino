ff630634434ecd970d3f27a7865ae448218301f5
CIT M <chakly_25@live.com.mx>
2019-07-02 21:27:46 -0500

    Restaurante Platino

--- Practica/Models/Formulario.cs
+++ Practica/Models/Formulario.cs
@@ -27,21 +27,25 @@ namespace Practica.Models
         public string apellidoM { get; set; }
 
         [Required(ErrorMessage = "El campo usuario es requerido")]
+        [StringLength(8, MinimumLength = 5, ErrorMessage = "El usuario es muy corto, intenta con otro")]
+
         [Display(Name = "Usuario")]
         public string usuario { get; set; }
 
         [Required(ErrorMessage = "El campo contraseña es requerido")]
-        
+        [StringLength(8, MinimumLength = 5, ErrorMessage = "La contraseña es muy debil, ingresa otra")]
+
         [Display(Name = "Contraseña")]
         public string contrasena { get; set; }
 
         [Required(ErrorMessage = "El campo telefono es requerido")]
-        [MinLength(8, ErrorMessage = "El telefono debe contener 8 digitos")]
-        [MaxLength(8, ErrorMessage = "El telefono debe contener 8 digitos")]
+       [Range (10000000,100000000, ErrorMessage ="El telefono ingresado debe contener 8 digitos")]
         [Display(Name = "Telefono")]
         public int telefono { get; set; }
 
         [Required(ErrorMessage = "El campo dirección es requerido")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "La dirección que ingresaste no es valida")]
+
         [Display(Name = "Dirección")]
         public string direccion { get; set; }
     
